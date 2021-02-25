b962622a95afe3f31bdd39ae7c2bef754be4ec7b
unknown <jaydenchen0000@gmail.com>
2021-02-24 23:27:57 -0500

    my CV

---
+++ precache-manifest.c039d78d870bc21c8def91ab31216cde.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173e7749190fc11304185a96c6ccf368",
+    "url": "/hub/index.html"
+  },
+  {
+    "revision": "c8c39e3b77fe89553d71",
+    "url": "/hub/static/css/main.e2e525c2.chunk.css"
+  },
+  {
+    "revision": "46d7782cb1b3815c3bdf",
+    "url": "/hub/static/js/2.371a855f.chunk.js"
+  },
+  {
+    "revision": "1ac4a8177a8d9f84c4732e5e50c7274f",
+    "url": "/hub/static/js/2.371a855f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c39e3b77fe89553d71",
+    "url": "/hub/static/js/main.51254cfb.chunk.js"
+  },
+  {
+    "revision": "073b6c4d21a6c3644bcb",
+    "url": "/hub/static/js/runtime-main.a7313f9b.js"
+  },
+  {
+    "revision": "9825b9206c349f975a53574393677889",
+    "url": "/hub/static/media/android.9825b920.jpg"
+  },
+  {
+    "revision": "b6cd0359dcbc1f1039c5154ecfa88410",
+    "url": "/hub/static/media/bg.b6cd0359.jpg"
+  },
+  {
+    "revision": "dacfcff6123f437f70ef27685fde1f3e",
+    "url": "/hub/static/media/collection.dacfcff6.jpeg"
+  },
+  {
+    "revision": "b700e382ff64fe235345beeb92588a2b",
+    "url": "/hub/static/media/cv.b700e382.png"
+  },
+  {
+    "revision": "1610a0ff82fcaf4f3ea5ba61b94dfc29",
+    "url": "/hub/static/media/dessert.1610a0ff.jpg"
+  },
+  {
+    "revision": "9c57ca54d5024efe64b91bf13a477dfe",
+    "url": "/hub/static/media/exhibition.9c57ca54.jpg"
+  },
+  {
+    "revision": "b57947e7dc308d2a619e7ebd6c495b12",
+    "url": "/hub/static/media/jsp1.b57947e7.JPG"
+  },
+  {
+    "revision": "32d2f75fe635b94625d543e955eb5854",
+    "url": "/hub/static/media/jsp2.32d2f75f.JPG"
+  },
+  {
+    "revision": "45d320c2c72622dc39f5aa1d16a9d3bb",
+    "url": "/hub/static/media/jsp3.45d320c2.JPG"
+  },
+  {
+    "revision": "11632f5e19af58190ce3f5b078d9a95e",
+    "url": "/hub/static/media/jsp4.11632f5e.JPG"
+  },
+  {
+    "revision": "91b58d338fa6b443e02e53bc6e00443a",
+    "url": "/hub/static/media/logo.91b58d33.png"
+  },
+  {
+    "revision": "88c85b522494c7a502d0787a23582ef5",
+    "url": "/hub/static/media/mern1.88c85b52.jpg"
+  },
+  {
+    "revision": "6450a59f4dec06979b39ba4fc72f2c23",
+    "url": "/hub/static/media/mern2.6450a59f.jpg"
+  },
+  {
+    "revision": "3e38774fa1a73b31e355692b31916460",
+    "url": "/hub/static/media/mern3.3e38774f.jpg"
+  },
+  {
+    "revision": "040594a68230edf90bf6c204b417323e",
+    "url": "/hub/static/media/mern4.040594a6.jpg"
+  },
+  {
+    "revision": "68937c258c51acff1e410253ab097a24",
+    "url": "/hub/static/media/mern5.68937c25.jpg"
+  },
+  {
+    "revision": "c36fa3d0c18d350d04fbd14ee38ba583",
+    "url": "/hub/static/media/mern6.c36fa3d0.jpg"
+  },
+  {
+    "revision": "11f51dad8dab29d398c31f3d5fd52df6",
+    "url": "/hub/static/media/mern7.11f51dad.jpg"
+  },
+  {
+    "revision": "ec1e8994bbcb5ef7bc1b585dda21b583",
+    "url": "/hub/static/media/micro1.ec1e8994.JPG"
+  },
+  {
+    "revision": "ecc415eed339b19dd4eb5b267829f403",
+    "url": "/hub/static/media/micro2.ecc415ee.JPG"
+  },
+  {
+    "revision": "171d8e1d5c472d7e848f79675602c759",
+    "url": "/hub/static/media/micro3.171d8e1d.JPG"
+  },
+  {
+    "revision": "c6453453868173ec8a3efd222d910fba",
+    "url": "/hub/static/media/python1.c6453453.jpg"
+  },
+  {
+    "revision": "8f22a2cfa8c78baa523bdb92b4723d7c",
+    "url": "/hub/static/media/python2.8f22a2cf.jpg"
+  },
+  {
+    "revision": "16ca0002786ad45ef302e074bbdcec87",
+    "url": "/hub/static/media/web.16ca0002.png"
+  }
+]);
